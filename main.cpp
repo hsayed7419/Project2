7705d3a77d98deee5b851a925577e8f8dd4dca68
Isaac Lemus <ilemus@mobis-usa.com>
2016-03-14 19:19:00 -0400

    [Isaac]  - Finalized reading from file  - Issue: Reading from file using infile >> line removed all whitepace    characters which were necessary for parsing  - Resolution: Reading file character by character and storing into a    string. String is finalized when '\n' is read

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,20 +9,44 @@ struct Book {
     string Title;
     float cost;
     int Type;
-}
+};
 
 struct Author {
     string First_name;
     string Middle_name;
     string Last_name;
-}
+};
 
 struct Publication_date {
     int month;
     int day;
     int year;
+};
+
+bool parseAndStore(string line, struct Book book) {
+    for (
+    line.at(i)
+}
+
+void printStruct(struct Book book){
+    cout << book.Publication_date.day << endl;
 }
 
 int main(){
-    
+    string line;
+    ifstream infile;
+    // opens file to be read
+    infile.open("record.dat");
+    char c;
+    do {
+        infile.get(c);
+        if (c == '\n'){
+            //line has been created
+            cout << line << endl;
+            line = "";
+            continue;
+        }
+        line += c;
+    } while (c != EOF);
+    infile.close();
 }
