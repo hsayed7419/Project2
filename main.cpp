f570ea4c7988dcf71b95f6bd58e65f3a97ed1e74
Isaac Lemus <ilemus@mobis-usa.com>
2016-03-14 20:44:22 -0400

    [Isaac]  - Compatability with .dat format  - Struct functions to store and test data structure

--- main.cpp
+++ main.cpp
@@ -9,6 +9,30 @@ struct Book {
     string Title;
     float cost;
     int Type;
+    void storeToStruct(string data, int type){
+        switch (type){
+            case 0:
+            convertPubDate(data);
+            break;
+            case 1:
+            convertAuthor(data);
+            break;
+            case 2:
+            convertTitle(data);
+            break;
+            case 3:
+            convertCost(data);
+            break;
+            case 4:
+            convertType(data);
+            break;
+        }
+    }
+    bool isFull(){
+        if (Publication_date && Author && Title && cost && Type){
+            return true;
+        } else return false;
+    }
 };
 
 struct Author {
@@ -23,27 +47,44 @@ struct Publication_date {
     int year;
 };
 
-bool parseAndStore(string line, struct Book book) {
-    for (
-    line.at(i)
+bool parse(string line, struct Book book) {
+    string substring;
+    int dataCount = 0;
+    for (int i = 0; i < line.size(); i++){
+        if (line.at(i) == ' '){
+            //do something with the line
+            dataCount++;
+        } else {
+            substring += line.at(i);
+        }
+    }
 }
 
 void printStruct(struct Book book){
-    cout << book.Publication_date.day << endl;
+    
 }
 
 int main(){
-    string line;
+    string line, info;
     ifstream infile;
+    struct Book temp;
     // opens file to be read
     infile.open("record.dat");
     char c;
+    int count = 0;
     do {
         infile.get(c);
         if (c == '\n'){
-            //line has been created
-            cout << line << endl;
+            if (count == 4){
+                //line has been created
+                cout << " " << endl;
+                count = 0;
+                line = "";
+                continue;
+            }
+            temp.storeToStruct(line, count);
             line = "";
+            count++;
             continue;
         }
         line += c;
